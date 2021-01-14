8c1c6f08da59e0fc8925ced2147e08fd005b0e94
Maria Hucko <maria.hucko1@gmail.com>
2021-01-14 22:53:32 +0100

    add core functionality

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+plik = open('dic\slowa.txt', 'r').read()
+linie = plik.split('\n')
 
+palindromy = []
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def palindrom(linie):
+    for slowo in linie:
+        if slowo == slowo[::-1]:
+            palindromy.append(slowo)
+    return palindromy
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(palindrom(linie))
+print('W słowniku znajduje się ', len(palindromy), 'palindromów.')
