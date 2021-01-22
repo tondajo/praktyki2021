073b286c610b48863ef450ccf8ac96b9f40d452b
Maria Hucko <maria.hucko1@gmail.com>
2021-01-23 00:00:05 +0100

    add time counter

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 
+import time
+
 plik = open('dic\slowa.txt', 'r').read()
 linie = plik.split('\n')
 
 palindromy = []
 
 def palindrom(linie):
+    przed = time.perf_counter()
     for slowo in linie:
         if (slowo != ""):
             if slowo == slowo[::-1]:
                 palindromy.append(slowo)
-
-
+    po = time.perf_counter()
+    print(f"Znaleziono w {po - przed:0.4f} sekund")
     return palindromy
 
 print(palindrom(linie))
 print('W słowniku znajduje się ', len(palindromy), 'palindromów.')
-
 
