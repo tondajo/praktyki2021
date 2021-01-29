5db8e95dbc7a2c64e7e13049209c1f0211455e3c
Maria Hucko <maria.hucko1@gmail.com>
2021-01-29 22:44:57 +0100

    add reverse function

--- main.py
+++ main.py
@@ -3,26 +3,38 @@ import time
 linie = []
 palindromy = []
 
+
+def reverse(slowo):
+    odroconeslowo = ''
+    for litera in slowo:
+        odroconeslowo = litera + odroconeslowo
+    return odroconeslowo
+
+
 def palindrom(linie):
     with open('dic\slowa.txt', "r", encoding="utf-8") as plik:
         linie = [linie[:-(linie[-1] == '\n') or len(linie) + 1] for linie in plik]
-        przed = time.perf_counter()
         for slowo in linie:
-            if slowo != "":
-                if slowo == slowo[::-1]:
-                    palindromy.append(slowo)
-        po = time.perf_counter()
-        print(f"Znaleziono w {po - przed:0.4f} sekund")
+            if reverse(slowo) == slowo:
+                palindromy.append(slowo)
     return palindromy
 
 
 def main():
+    przed = time.perf_counter()
     print('Znalezione palindromy:', palindrom(linie))
     print('W słowniku znajduje się ', len(palindromy), 'palindromów.')
+    po = time.perf_counter()
+    print(f"Znaleziono w {po - przed:0.4f} sekund")
 
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
 
 
